f677993fba5e49f8dd2ad1db2226df88e2782e65
mfquinonesc <manuelfqc@gmail.com>
2025-05-12 08:35:45 -0500

    [12/05/2025 08:35] Update

--- chess/piece.py
+++ chess/piece.py
@@ -22,21 +22,55 @@ class Piece():
 
         return True    
 
+
+    def get_squares_between(self, row, col):
+
+        squares = list()
+
+        if self.is_valid_move(row, col):
+
+            dist = math.sqrt(row**2 + col**2)
+            current_dist = math.sqrt(self.row**2 + self.col**2)
+
+            row0 = row if dist < current_dist else self.row
+            row1 = self.row if dist < current_dist else row
+            
+            col0 = col if dist < current_dist else self.col
+            col1 = self.col if dist < current_dist else col
+
+            for x  in  range(col0, col1 + 1):
+                for y in range(row0 , row1 + 1):
+                    squares.append([y,x])
+
+            squares.remove([self.row, self.col])
+            squares.remove([row, col])
+            
+        return squares
+    
+
     def is_same_piece_as(self, piece):
         return type(self) == type(piece)
 
     def compare_to(self, piece):
         return (self.is_same_piece_as(piece) and self.row == piece.row and self.col == piece.col and self.color == piece.color)
+    
+    def get_name(self):
+        return str(type(self).__name__)
+    
+    def get_color(self):
+        return 'White' if self.color else 'Black'
 
     def __str__(self):
         return self.symbol.upper() if self.color else self.symbol.lower()
 
 
+
 class King(Piece):
 
-    def __init__(self, row, col, color=True):
+    def __init__(self, row, col, color):
         super().__init__(row, col, color)
         self.symbol = 'K'
+
 
     def is_valid_move(self, row, col):
 
@@ -45,8 +79,9 @@ class King(Piece):
         if (dist > math.sqrt(2)):
             return False
 
-        return super().is_valid_move(row, col)
-
+        return super().is_valid_move(row, col)    
+   
+  
 
 class Rook(Piece):
 
@@ -60,6 +95,7 @@ class Rook(Piece):
             return False
 
         return super().is_valid_move(row, col)
+
 
 
 class Bishop(Piece):
@@ -77,6 +113,17 @@ class Bishop(Piece):
             return False
 
         return super().is_valid_move(row, col)
+    
+    
+    def get_squares_between(self, row, col):
+        squares = super().get_squares_between(row, col)
+        filter = list()
+        for s in squares:
+           if self.is_valid_move(s[0], s[1]):
+               filter.append(s)
+
+        return filter
+
 
 
 class Queen(Piece):
@@ -94,6 +141,16 @@ class Queen(Piece):
         bishop_move = bishop.is_valid_move(row, col)
 
         return rook_move or bishop_move
+    
+    def get_squares_between(self, row, col):               
+        rook_squares = Rook(self.row, self.col, self.color).get_squares_between(row, col)
+        bishop_squares = Bishop(self.row, self.col, self.color).get_squares_between(row, col)
+        
+        if len(rook_squares) > 0:
+            return rook_squares
+        
+        return bishop_squares
+
 
 
 class Knight(Piece):
@@ -111,6 +168,10 @@ class Knight(Piece):
             return False
 
         return super().is_valid_move(row, col)
+    
+    def get_squares_between(self, row, col):
+        return list()
+
 
 
 class Pawn(Piece):
@@ -138,4 +199,4 @@ class Pawn(Piece):
         if (dist > math.sqrt(2)):
             return False
 
-        return super().is_valid_move(row, col)
+        return super().is_valid_move(row, col)    
