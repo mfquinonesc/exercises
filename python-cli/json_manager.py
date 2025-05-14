c8f13e83bf4da7b400d17c885b7dd89d77139cc6
mfquinonesc <manuelfqc@gmail.com>
2025-05-14 16:39:55 -0500

    [14/05/2025 16:39] Update

---
+++ python-cli/json_manager.py
@@ -0,0 +1,19 @@
+import os 
+import json 
+
+def read_json():
+    if not os.path.isfile('data.json'):
+        with open('data.json','w') as f:
+            json.dump([],f)
+
+    with open('data.json','r') as f:
+       data = json.load(f)
+
+    return data
+
+def write_json(data):
+    with open('data.json', 'w') as f:
+        json.dump(data,f)
+
+
+    pass 
